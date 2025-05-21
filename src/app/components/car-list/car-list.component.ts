5c4f3aa75916a95c473585c98b1316aae5d42d6f
nada <95084271+JdNada@users.noreply.github.com>
2025-05-21 02:48:13 +0200

    add part frontend

--- src/app/components/car-list/car-list.component.ts
+++ src/app/components/car-list/car-list.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService, Car } from '../../services/car.service';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.css'],
+  imports: [CommonModule]
 })
 export class CarListComponent implements OnInit {
   cars: Car[] = [];
